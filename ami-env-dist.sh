0224a753c42261a9b2f8bd60342db9ac03eccc52
Charles Severance <csev@umich.edu>
2020-04-16 15:04:14 -0400

    Updates

--- ami-env-dist.sh
+++ ami-env-dist.sh
@@ -1,7 +1,12 @@
 
 # Copy to ami-env.sh and edit
 
+# export APACHE_SERVER_NAME=test.pg4e.com
+# export TSUGI_APPHOME=http://test.pg4e.com
+
+export APACHE_SERVER_NAME=localhost
 export TSUGI_APPHOME=http://localhost:8080 
+
 export TSUGI_SERVICENAME=PG4E 
 export MYSQL_ROOT_PASSWORD=secret 
 export MAIN_REPO=https://github.com/csev/pg4e 
